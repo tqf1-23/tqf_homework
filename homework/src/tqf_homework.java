e04927619739a9a6bd64c34e1a6b8c0cb7872bfa
“tqf” <285733466@qq.com>
2020-03-04 22:18:55 +0800

    First commit

---
+++ homework/src/tqf_homework.java
@@ -0,0 +1,14 @@
+public class tqf_homework {
+
+    public static void main(String[] args)
+    {
+        int result;
+        for(int i=1;i<=13;i++){
+            result=Fibonacci.of(i);
+            System.out.println(result);
+        }
+
+    }
+
+
+}
